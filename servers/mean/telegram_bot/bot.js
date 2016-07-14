aa471e1d929db3f2eed0ba6d2886fd2845c089d3
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-15 00:55:53 +0430

    some refactoring

--- servers/mean/telegram_bot/bot.js
+++ servers/mean/telegram_bot/bot.js
@@ -7,20 +7,27 @@ var Bot = require('node-telegram-bot-api'),
 console.log('bot server started...');
 
 bot.onText(/^\/sir1 (.+)$/, (msg, match) => {
-    var movie_title = match[1];
+
+    var movie_title = match[1].replace(/\s/g, '+');
+
+    var chatId = msg.chat.id;
+    // From file
+    var photo = __dirname + '/public/movie.png';
+    bot.sendPhoto(chatId, photo, { caption: "Hi man, let's search a movie by its Title" });
 
     movieService.getMovieByTitle(movie_title, (err, result) => {
         var res = result;
         var template = `
             Title: ${res.Title}
-        Year:  ${res.Year}
-        Rated: ${res.Rated}
-        Released: ${res.Released}
-        Runtime: ${res.Runtime}
-        Genre:   ${res.Genre}
-        Director: ${res.Director}
-        Plot:     ${res.Plot}
+            Year:  ${res.Year}
+            Rated: ${res.Rated}
+            Released: ${res.Released}
+            Runtime: ${res.Runtime}
+            Genre:   ${res.Genre}
+            Director: ${res.Director}
+            Plot:     ${res.Plot}
         `;
+        //bot.sendPhoto(msg.chat.id, photo, {caption: result.Title });
 
         bot.sendMessage(msg.chat.id, template).then(() => {
             // reply sent!
