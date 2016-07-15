2008cdc5220ce9e11f139f3e0867e539d3127fa1
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-15 13:47:45 +0430

    Added csharp collection samples

---
+++ languages/csharp/Collections/BasicArray/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BasicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monday = "Monday";
+            string[] daysOfWeek = {
+                                        "Monday",
+                                        "Tuesday",
+                                        "Wednesday",
+                                        "Thursday",
+                                        "Friday",
+                                        "Saturday",
+                                        "Sunday"
+                                    };
+            System.Console.WriteLine(daysOfWeek[8]);
+            daysOfWeek[5] = "PartyDay";
+            foreach (var day in daysOfWeek)
+            {
+                System.Console.WriteLine(day);
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
