965146212659807e6d50b723aaff5db8320d0b09
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-17 19:10:33 +0430

    copying array in .NET

---
+++ languages/csharp/Collections/ArrayCopy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] squares = { 1, 4, 9, 16 };
+
+            int[] copy = squares.ToArray();
+
+            foreach (var value in copy)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine(string.Format(
+                "squares == copy? {0}", squares == copy));
+
+            Console.ReadLine();
+        }
+    }
+}
