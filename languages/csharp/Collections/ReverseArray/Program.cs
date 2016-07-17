e3859603c1e596faab1544a470d1d38d2ed3abb3
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-18 00:30:18 +0430

    using array sort - csharp

---
+++ languages/csharp/Collections/ReverseArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = 
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            var reverse = daysOfWeek.Reverse();
+
+            //Array.Reverse(daysOfWeek);
+
+            foreach (string day in daysOfWeek)
+                Console.WriteLine(day);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ languages/csharp/Collections/SortArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            //var comparer = new StringLengthComparer();
+
+            var orderedEnumerable = daysOfWeek.OrderBy(p => p.Length);
+
+            //Array.Sort(daysOfWeek, comparer);
+
+            foreach (string day in orderedEnumerable)
+                Console.WriteLine(day);
+
+            Console.ReadLine();
+        }
+    }
+
+    class StringLengthComparer : IComparer<String>
+    {
+        public int Compare(string x, string y)
+        {
+            return x.Length.CompareTo(y.Length);
+        }
+    }
+}
