14eff79ff5cce02b857403e8ec80cf4ac1f6d1a0
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-18 20:08:13 +0430

    Finding items in array - csharp

---
+++ languages/csharp/Collections/FindElements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FindElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                                        "Monday",
+                                        "Tuesday",
+                                        "Wednesday",
+                                        "Thursday",
+                                        "Friday",
+                                        "Saturday",
+                                        "Sunday"
+                                    };
+            //int indexOfTues = Array.IndexOf(daysOfWeek, "Tuesday");
+            /*int indexOfW = Array.FindIndex(daysOfWeek, x => x[0] == 'W');
+            Console.WriteLine(indexOfW);
+            Console.WriteLine(daysOfWeek[indexOfW]);*/
+            string[] allWith6Chars = Array.FindAll(
+                daysOfWeek, x => x.Length == 6);
+            foreach (string item in allWith6Chars)
+                Console.WriteLine(item);
+
+            Console.ReadLine();
+        }
+    }
+}
