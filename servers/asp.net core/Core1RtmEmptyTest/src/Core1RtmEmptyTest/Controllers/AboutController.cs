17420c51987069627b5e8f7d09bd5e8718bcea09
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-19 12:46:56 +0430

    using routing in ASP.NET Core

---
+++ servers/asp.net core/Core1RtmEmptyTest/src/Core1RtmEmptyTest/Controllers/AboutController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core1RtmEmptyTest.Controllers
+{
+    [Route("[controller]")]
+    public class AboutController : Controller
+    {
+        [Route("")]
+        public ActionResult Hello()
+        {
+            return Content("Hello from DNT!");
+        }
+        [Route("[action]")]
+        public ActionResult SiteName()
+        {
+            return Content("DNT");
+        }
+
+        [Route("Users/{userid:int?}", Name = "GetUserById")]
+        public IActionResult GetUsers(int userId)
+        {
+            return Json(new {userId = userId});
+        }
+    }
+}
--- servers/asp.net core/Core1RtmEmptyTest/src/Core1RtmEmptyTest/Startup.cs
+++ servers/asp.net core/Core1RtmEmptyTest/src/Core1RtmEmptyTest/Startup.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
 using StructureMap;
 
 namespace Core1RtmEmptyTest
@@ -18,7 +20,7 @@ namespace Core1RtmEmptyTest
 
             //services.Configure<SmtpConfig>(options => Configuration.GetSection("Smtp").Bind(options));
 
-            services.AddDirectoryBrowser();
+            //services.AddDirectoryBrowser();
 
             //services.AddSingleton<IConfiguration>(provider => { return Configuration; });
             //services.AddTransient<IMessagesService, MessagesService>();
@@ -29,8 +31,28 @@ namespace Core1RtmEmptyTest
                               IHostingEnvironment env)
         {
 
+            // Serve wwwroot as root
             app.UseFileServer();
-            app.UseMvcWithDefaultRoute();
+
+            // Serve /node_modules as a separate root (for packages that use other npm modules client side)
+            app.UseFileServer(new FileServerOptions
+            {
+                // Set root of file server
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
+                // Only react to requests that match this path
+                RequestPath = "/node_modules",
+                // Don't expose file system
+                EnableDirectoryBrowsing = false
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute("spa-fallback", new { controller = "Home", action = "Index" });
+            });
         }
     }
 }
