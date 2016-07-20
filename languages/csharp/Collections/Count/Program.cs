2a51e5a83e3c63d97c500033a5d222e9b6da7b6c
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-20 18:07:49 +0430

    using count for collections c#

---
+++ languages/csharp/Collections/Count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek =  {
+                                        "Monday",
+                                        "Tuesday",
+                                        "Wednesday",
+                                        "Thursday",
+                                        "Friday",
+                                        "Saturday",
+                                        "Sunday"
+                                    };
+            ICollection<string> collection = 
+                (ICollection<string>) daysOfWeek;
+            Console.WriteLine("Count() = " + collection.Count());
+            Console.WriteLine("Count = " + daysOfWeek.Length);
+            Console.WriteLine("Length = " + daysOfWeek.Length);
+            Console.ReadLine();
+        }
+    }
+}
