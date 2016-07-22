098ca8fc15a7e0f3fbfa3ec3c298931f868b5756
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-22 22:51:07 +0430

    Using ObservableCollection

---
+++ languages/csharp/Collections/ObservableCollection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace ObservableCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<string> presidents = new
+                ObservableCollection<string>
+            {
+                "Jimmy Carter",
+                "Ronald Regan",
+                "Georg HW Bush"
+            };
+            presidents.CollectionChanged += OnCollectionChanged;
+
+            presidents.Add("Bill Clinton");
+            presidents.Remove("Jimmy Carter");
+
+            foreach (string president in presidents)
+                Console.WriteLine(president);
+
+
+            Console.ReadLine();
+        }
+
+        static void OnCollectionChanged(object sender,
+            NotifyCollectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
