1879fa354bc0bbaf86e468e36934d94a96d020e4
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-23 14:13:47 +0430

    started the book bot

--- servers/mean/telegram_bot_books/bot.js
+++ servers/mean/telegram_bot_books/bot.js
@@ -2,7 +2,7 @@ var token = '254779549:AAE_LXWqxmng7TXytKlAyMt9NFgIQg8lstU';
 
 var Bot = require('node-telegram-bot-api'),
     bot = new Bot(token, { polling: true }),
-    bookService = require('./services/bookService.js'),
+    movieService = require('./services/bookService.js'),
     moment = require('moment-jalaali'),
     fs = require('fs');
 
@@ -12,8 +12,10 @@ var results = [];
 
 bot.on('inline_query', function (query) {
 
-    var book_title = match[1].replace(/\s/g, '+');
+    var book_title = query.query.replace(/\s/g, '+');
 
+    console.log(book_title);
+    
     if (book_title === "") 
         results = [];
 
@@ -22,8 +24,28 @@ bot.on('inline_query', function (query) {
     movieService.getBookByTitle(book_title, (err, result) => {
 
         var res = result;
+        var image = res[0].best_book;
+        var template = `
+عنوان: ""
+        `;
 
-        
+        console.log(image);
+
+        var number = Math.floor(Math.random() * 64) + 1;
+            
+            results.push({
+                type: "photo",
+                id: number.toString(),
+                photo_url: image.toString(),
+                thumb_url: image.toString(),
+                title: "",
+                description: "",
+                caption:"",
+                input_message_content: {
+                    message_text: template
+                }
+            });
+            bot.answerInlineQuery(query.id, results);
     });
 
 
