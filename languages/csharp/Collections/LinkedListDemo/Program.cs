b8f8fd5bfbe627236efcfcdc67b5d684c1d4e98c
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-23 17:55:24 +0430

    Using LinkedList

---
+++ languages/csharp/Collections/LinkedListDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var presidents = new LinkedList<string>();
+            presidents.AddLast("JFK");
+            presidents.AddLast("Lyndon B Johnson");
+            presidents.AddLast("Richard Nixon");
+            presidents.AddLast("Jimmy Carter");
+
+            presidents.RemoveFirst();
+
+            LinkedListNode<string> item = presidents.Find("Richard Nixon");
+
+            presidents.AddAfter(item, "Gerlad Ford");
+
+            foreach (string president in presidents)
+                Console.WriteLine(president);
+
+            Console.ReadLine();
+        }
+    }
+}
