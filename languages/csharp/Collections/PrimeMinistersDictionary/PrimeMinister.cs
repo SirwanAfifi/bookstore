4ddaa600309626da20729c16b2dbd233dc5b0704
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-25 16:47:48 +0430

    Using Dictionary<TKey, TValue> - C#

---
+++ languages/csharp/Collections/PrimeMinistersDictionary/PrimeMinister.cs
@@ -0,0 +1,19 @@
+﻿namespace PrimeMinistersDictionary
+{
+    public class PrimeMinister
+    {
+        public string Name { get; set; }
+        public int YearElected { get; set; }
+
+        public PrimeMinister(string name, int yearElected)
+        {
+            this.Name = name;
+            this.YearElected = yearElected;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, elected: {YearElected}";
+        }
+    }
+}
---
+++ languages/csharp/Collections/PrimeMinistersDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeMinistersDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeMinisters = new Dictionary<string, PrimeMinister>
+            {
+                { "JC", new PrimeMinister("James Callaghan", 1976) },
+                { "MT", new PrimeMinister("Margaret Thatcher", 1979) },
+                { "TB", new PrimeMinister("Tony Blair", 1997) }
+            };
+
+            foreach (var pm in primeMinisters)
+                Console.WriteLine(pm);
+
+            Console.ReadLine();
+        }
+    }
+}
