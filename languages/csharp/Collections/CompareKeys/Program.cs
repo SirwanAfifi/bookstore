b9d09989c6d596d00bbf8945eaeae29d8a267a67
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-27 16:33:22 +0430

    Using StringComparer for a dictionary

---
+++ languages/csharp/Collections/CompareKeys/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompareKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeMinisters = new Dictionary<string, PrimeMinister>
+            (StringComparer.InvariantCultureIgnoreCase)
+            {
+                { "JC", new PrimeMinister("James Callaghan", 1974) },
+                { "MT", new PrimeMinister("Margaret Thatcher", 1979) },
+                { "TB", new PrimeMinister("Tony Blair", 1997) }
+            };
+
+            Console.WriteLine(primeMinisters["tb"]);
+
+
+            Console.ReadLine();
+        }
+    }
+}
