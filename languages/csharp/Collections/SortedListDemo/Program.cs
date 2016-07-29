4da8fb0227a197f7aacf80e348ef8574a49c7984
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-29 20:27:28 +0430

    Using SortedList

---
+++ languages/csharp/Collections/SortedListDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeMinisters = new SortedList<string, PrimeMinister>
+            {
+                { "JC", new PrimeMinister("James Callaghan", 1974) },
+                { "MT", new PrimeMinister("Margaret Thatcher", 1979) },
+                { "TB", new PrimeMinister("Tony Blair", 1997) }
+            };
+            primeMinisters.Add("JM", new PrimeMinister("John Major", 1990));
+
+            foreach (var pm in primeMinisters)
+                Console.WriteLine(pm);
+
+            Console.ReadLine();
+
+        }
+    }
+}
