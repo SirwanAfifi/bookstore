079af1238fac9f1188ec02023ca3e1228273174e
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-31 18:01:32 +0430

    Using StringComparer for HashSet

---
+++ languages/csharp/Collections/HashSetComparer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new HashSet<string>(StringComparer.CurrentCultureIgnoreCase)
+            { "New York", "Manchester", "Sheffield", "Paris" };
+
+            bigCities.Add("SHEFFIELD");
+            bigCities.Add("BEIJING");
+
+            foreach (string city in bigCities)
+                Console.WriteLine(city);
+
+            Console.ReadLine();
+        }
+    }
+}
