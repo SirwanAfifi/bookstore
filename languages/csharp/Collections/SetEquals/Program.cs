4bab7d03c08da67863d19a4b5272ff3f513d859b
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-31 19:05:59 +0430

    Using SetEquals for two Sets

---
+++ languages/csharp/Collections/SetEquals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new HashSet<string>
+            { "New York", "Manchester", "Sheffield", "Paris" };
+
+            var citiesInUk = new HashSet<string>
+            { "Sheffield", "Ripon", "Truro", "Manchester" };
+
+            var bigCitiesArr = new string[]
+            { "New York", "Manchester", "Sheffield", "Paris" };
+
+            bool areEqual = bigCities.SetEquals(bigCitiesArr);
+            Console.WriteLine("bigCities = bigCitiesArr? " + areEqual);
+
+            bool areEqualUk = citiesInUk.SetEquals(bigCitiesArr);
+            Console.WriteLine("citiesInUk = bigCitiesArr? " + areEqualUk);
+
+            Console.ReadLine();
+        }
+    }
+}
