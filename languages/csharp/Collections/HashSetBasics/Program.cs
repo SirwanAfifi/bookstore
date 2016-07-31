d07a82ddd6f7dec926075505d2723514085a028f
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-31 17:42:45 +0430

    Without using HashSet<T>

---
+++ languages/csharp/Collections/HashSetBasics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new List<string>
+            { "New York", "Manchester", "Sheffield", "Paris" };
+
+            if (!bigCities.Contains("Shefield"))
+                bigCities.Add("Shefield");
+            if (!bigCities.Contains("Beijing"))
+                bigCities.Add("Beijing");
+
+            foreach (string city in bigCities)
+                Console.WriteLine(city);
+
+            Console.ReadLine();
+        }
+    }
+}
