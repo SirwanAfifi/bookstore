5d9db76d2467efbed6d155b56ef1aa1b900ced51
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-07-31 18:51:00 +0430

    Using UnionWith

--- languages/csharp/Collections/Intersect/Program.cs
+++ languages/csharp/Collections/Intersect/Program.cs
@@ -14,13 +14,9 @@ namespace Intersect
             string[] citiesInUk =
             { "Sheffield", "Ripon", "Truro", "Manchester" };
 
-            var newCities = bigCities.Intersect(citiesInUk);
-            foreach (string city in newCities)
-                Console.WriteLine(city);
-
-            /*bigCities.IntersectWith(citiesInUk);
+            bigCities.UnionWith(citiesInUk);
             foreach (string city in bigCities)
-                Console.WriteLine(city);*/
+                Console.WriteLine(city);
 
             Console.ReadLine();
         }
