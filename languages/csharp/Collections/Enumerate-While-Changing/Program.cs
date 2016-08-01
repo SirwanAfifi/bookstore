f54cc1a5a07a073cf4eba5652ef919fc906c4561
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-01 19:16:22 +0430

    Enumerate_While_Changing

---
+++ languages/csharp/Collections/Enumerate-While-Changing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enumerate_While_Changing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var days = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday"
+            };
+
+            foreach (string day in days)
+            {
+                days[1] = "2nd day";
+                Console.WriteLine(day);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
