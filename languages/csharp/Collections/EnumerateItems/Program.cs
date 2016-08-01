6e303976680cbe1956017dc06bfc35de660c53f2
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-01 18:05:21 +0430

    Behind the scene of foreach

---
+++ languages/csharp/Collections/EnumerateItems/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumerateItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                            "Monday",
+                            "Tuesday",
+                            "Wednesday",
+                            "Thursday",
+                            "Friday",
+                            "Saturday",
+                            "Sunday"
+                        };
+
+            DisplayItems(daysOfWeek);
+
+            Console.ReadLine();
+        }
+
+        public static void DisplayItems<T>(IEnumerable<T> collection)
+        {
+            using (IEnumerator<T> enumerator = collection.GetEnumerator())
+            {
+                bool moreItems = enumerator.MoveNext();
+                while (moreItems)
+                {
+                    T item = enumerator.Current;
+                    Console.WriteLine(item);
+                    moreItems = enumerator.MoveNext();
+                }
+            }
+        }
+    }
+}
