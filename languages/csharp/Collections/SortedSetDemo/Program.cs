c190cfb23524e852099ff0d1c892bd0748ff29da
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-01 17:41:41 +0430

    Using SortedSet

---
+++ languages/csharp/Collections/SortedSetDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new SortedSet<string>(new UncasedStringComparer())
+            { "New York", "Manchester", "Sheffield", "Paris" };
+
+            bigCities.Add("Sheffield");
+            bigCities.Add("Beijing");
+
+            foreach (string city in bigCities)
+                Console.WriteLine(city);
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class UncasedStringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y, 
+                StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
