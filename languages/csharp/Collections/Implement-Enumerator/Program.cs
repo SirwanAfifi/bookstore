55c10e27b2a8fcd0c1b4cfe7124c2b6a54d2c970
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-03 00:31:17 +0430

    Writing custom Enumerator

---
+++ languages/csharp/Collections/Implement-Enumerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Implement_Enumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AllDaysOfWeek allDays = new AllDaysOfWeek();
+            foreach (string allDay in allDays)
+                Console.WriteLine(allDay);
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class AllDaysOfWeek : IEnumerable<string>
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            Console.WriteLine("Calling generic GetEnumerator");
+            yield return "Monday";
+            yield return "Tuesday";
+            yield return "Wednesday";
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
