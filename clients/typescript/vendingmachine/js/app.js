f0741c2bb7f292219e3842c9ff484c49a9fcd626
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-03 22:57:52 +0430

    connecting the UI - typescript app

--- clients/typescript/vendingmachine/js/app.js
+++ clients/typescript/vendingmachine/js/app.js
@@ -1,17 +1,38 @@
-var VendingMachine = (function () {
-    function VendingMachine() {
-    }
-    return VendingMachine;
-}());
-/// <reference path="vendingMachine.ts" />
-var machine = new VendingMachine();
 var Quarter = (function () {
     function Quarter() {
         this.value = .25;
     }
+    Object.defineProperty(Quarter.prototype, "Value", {
+        get: function () {
+            return this.value;
+        },
+        set: function (newValue) {
+            this.value = newValue;
+        },
+        enumerable: true,
+        configurable: true
+    });
     Quarter.prototype.getImageUrl = function () {
         return "img/Quarter.png";
     };
     return Quarter;
 }());
+var coin = new Quarter();
+var value = coin.Value;
+coin.Value = 0.25;
+/// <reference path="./coin.ts" />
+var VendingMachine = (function () {
+    function VendingMachine() {
+        var _this = this;
+        this.paid = 0;
+        this.acceptCoin = function (coin) {
+            _this.paid = _this.paid + coin.Value;
+            var element = document.getElementById("total");
+            element.innerHTML = _this.paid.toString();
+        };
+    }
+    return VendingMachine;
+}());
+/// <reference path="vendingMachine.ts" />
+var machine = new VendingMachine();
 //# sourceMappingURL=app.js.map
