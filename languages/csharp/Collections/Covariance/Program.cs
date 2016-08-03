763a534c632cb5db17b3139bbcdeed1cb1e50015
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-03 18:08:49 +0430

    Enumerable Convariance

---
+++ languages/csharp/Collections/Covariance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Covariance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello, World!";
+            object obj = str;
+
+            var strList = new List<string> { "Monday", "Tuesday" };
+            IEnumerable<object> objEnum = strList;
+
+            /*
+               var strList = new string[] { "Monday", "Tuesday" };
+               object[] objectList = strList;
+             */
+
+            Console.ReadLine();
+        }
+    }
+}
