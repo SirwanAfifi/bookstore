1344beba143bb86dbc8a20bb071a196ff5665ec5
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-03 18:47:52 +0430

    Using Multidimensional Arrays

---
+++ languages/csharp/Collections/MultiDimensionalArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MultiDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[,] tempsGrid = new float[4, 3];
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    tempsGrid[x, y] = x + 10*y;
+                }
+            }
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Console.Write(tempsGrid[x, y] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
