c40ee100d2d65a489b2d61d9827a1dfa28360cab
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-05 02:14:41 +0430

    Using Jagged Arrays

---
+++ languages/csharp/Collections/JaggedArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[][] tempsGrid = new float[4][];
+
+            for (int x = 0; x < 4; x++)
+            {
+                // مقداردهی آرایه
+                tempsGrid[x] = new float[3];
+
+                for (int y = 0; y < 3; y++)
+                {
+                    tempsGrid[x][y] = x + 10 * y;
+                }
+            }
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Console.Write(tempsGrid[x][y] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
