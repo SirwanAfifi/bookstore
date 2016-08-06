6250cf70c729fd530bd043bad1713fcf8f1661bd
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-06 18:07:08 +0430

    Programming to interfaces - csharp

---
+++ languages/csharp/Interfaces/PeopleViewer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+using PersonLibrary;
+
+namespace PeopleViewer
+{
+    public partial class MainWindow : Window
+    {
+        readonly PeopleRepository _peopleRepo = new PeopleRepository();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConcreteFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+
+            List<Person> people;
+            people = _peopleRepo.GetPeople();
+            PersonListBox.ItemsSource = people;
+            //foreach (var person in people)
+            //    PersonListBox.Items.Add(person);
+        }
+
+        private void InterfaceFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+
+            IEnumerable<Person> people;
+            people = _peopleRepo.GetPeople();
+            PersonListBox.ItemsSource = people;
+            //foreach (var person in people)
+            //    PersonListBox.Items.Add(person);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ClearListBox()
+        {
+            //PersonListBox.Items.Clear();
+            PersonListBox.ItemsSource = null;
+        }
+    }
+
+}
