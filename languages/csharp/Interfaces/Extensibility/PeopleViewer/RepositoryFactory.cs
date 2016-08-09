89dbc93756496c265c4e8ccd90d13510534d6840
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-09 18:38:12 +0430

    Adding repository pattern

---
+++ languages/csharp/Interfaces/Extensibility/PeopleViewer/RepositoryFactory.cs
@@ -0,0 +1,12 @@
+﻿using PersonRepository.Interface;
+
+namespace PeopleViewer
+{
+    public static class RepositoryFactory
+    {
+        public static IPersonRepository GetRepository(string repositoryType)
+        {
+            return null;
+        }
+    }
+}
