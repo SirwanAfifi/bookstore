2bd8c8b73949b185f5c5bc57d8904954c0dd2d63
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-16 17:47:37 +0430

    added unit tests

---
+++ languages/csharp/Interfaces/DynamicLoading/PeopleViewer.Test/RepositoryFactoryTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersonRepository.Fake;
+using PersonRepository.Interface;
+
+namespace PeopleViewer.Test
+{
+    [TestClass]
+    public class RepositoryFactoryTest
+    {
+        [TestMethod]
+        public void GetRepository_OnCreation_ReturnFakeRepository()
+        {
+            // Arrange / Act
+            var repositoryFactory = RepositoryFactory.GetRepository();
+
+            // Assert
+            Assert.AreEqual(repositoryFactory.GetType(), typeof(FakeRepository));
+        }
+    }
+}
