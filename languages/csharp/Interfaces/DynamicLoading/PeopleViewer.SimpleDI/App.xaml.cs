28e00a259b20ca7011fcdce8d9ea22989db2be9e
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-19 20:09:26 +0430

    Added simple DI container

---
+++ languages/csharp/Interfaces/DynamicLoading/PeopleViewer.SimpleDI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PersonRepository.Fake;
+using PersonRepository.Interface;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            FakeRepository repository = new FakeRepository();
+            SimpleContainer.MapInstance<IPersonRepository>(repository);
+        }
+    }
+}
