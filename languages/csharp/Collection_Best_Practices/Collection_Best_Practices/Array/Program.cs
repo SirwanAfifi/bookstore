8cd71354168c4134501040cd3cc57125589cc3ec
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-26 12:51:07 +0430

    Iterating array

---
+++ languages/csharp/Collection_Best_Practices/Collection_Best_Practices/Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] colorOptions = { "Red", "Espresso", "White", "Navy" };
+
+            for (int i = 0; i < colorOptions.Length; i++)
+            {
+                colorOptions[i] = colorOptions[i].ToLower();
+            }
+
+            foreach (var color in colorOptions)
+            {
+                Console.WriteLine($"The color is {color}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
