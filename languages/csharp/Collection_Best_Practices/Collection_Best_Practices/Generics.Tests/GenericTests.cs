c2e81be4bdb073643eba25a00259e810ca722bd7
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-08-28 20:03:34 +0430

    Added generic methods

---
+++ languages/csharp/Collection_Best_Practices/Collection_Best_Practices/Generics.Tests/GenericTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Tests
+{
+    [TestClass()]
+    public class GenericTests
+    {
+        [TestMethod()]
+        public void RetrieveValueTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = 42;
+
+            //Act
+            var actual = repository.RetrieveValue<int>("Select .....", 42);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RetrieveValueStringTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = "test";
+
+            //Act
+            var actual = repository.RetrieveValue<string>("Select ....", "test");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
