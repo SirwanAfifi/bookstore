be84d906b5a09561760ef044ae6014514254a0b6
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-09-01 10:52:31 +0430

    added owin project

---
+++ servers/asp.net/Owin.Demo/Owin.Demo/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace Owin.Demo
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("<html><head></head><body><h1>Hello World</h1></body></html>");
+            });
+        }
+    }
+}
