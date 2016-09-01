102a0708af817d36290fbb09f415cdf9be22e61a
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-09-01 11:01:59 +0430

    added next middleware

--- servers/asp.net/Owin.Demo/Owin.Demo/Startup.cs
+++ servers/asp.net/Owin.Demo/Owin.Demo/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
@@ -9,6 +10,14 @@ namespace Owin.Demo
     {
         public static void Configuration(IAppBuilder app)
         {
+            app.Use(async (ctx, next) =>
+            {
+                Debug.WriteLine("Incoming request: " + ctx.Request.Path);
+                await next();
+                Debug.WriteLine("Outgoing request: " + ctx.Request.Path);
+            });
+
+
             app.Use(async (ctx, next) =>
             {
                 await ctx.Response.WriteAsync("<html><head></head><body><h1>Hello World</h1></body></html>");
