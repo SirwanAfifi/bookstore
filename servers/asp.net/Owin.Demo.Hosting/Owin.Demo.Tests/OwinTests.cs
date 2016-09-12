e2c00e379d2fb668efb19d35b5f35e3a0b295a1b
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-09-13 00:23:05 +0430

    DRY

--- servers/asp.net/Owin.Demo.Hosting/Owin.Demo.Tests/OwinTests.cs
+++ servers/asp.net/Owin.Demo.Hosting/Owin.Demo.Tests/OwinTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Owin.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,32 +14,41 @@ namespace Owin.Demo.Tests
         [TestMethod]
         public async Task Owin_returns_200_on_request_to_root()
         {
-            using (var server = TestServer.Create<Startup>())
-            {
-                var response = await server.HttpClient.GetAsync("/");
-                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-            }
+            var response = await CallServer(async x => await x.GetAsync("/"));
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
         [TestMethod]
         public async Task Owin_returns_hello_world_on_request_to_root()
         {
-            using (var server = TestServer.Create<Startup>())
+            var body = await CallServer(async x =>
             {
-                var response = await server.HttpClient.GetAsync("/");
-                var body = await response.Content.ReadAsStringAsync();
-                Assert.AreEqual("Hello World", body);
-            }
+                var response = await x.GetAsync("/");
+                return await response.Content.ReadAsStringAsync();
+            });
+
+            Assert.AreEqual("Hello World", body);
         }
 
         [TestMethod]
         public async Task Owin_returns_correct_contenttype_on_request_to_jpg()
         {
+            var contenttype = await CallServer(async x =>
+            {
+                var response = await x.GetAsync("/Me.jpg");
+                return response.Content.Headers.ContentType.MediaType;
+                
+            });
+
+            Assert.AreEqual("image/jpeg", contenttype);
+        }
+
+        private async Task<T> CallServer<T>(Func<HttpClient, Task<T>> callback)
+        {
             using (var server = TestServer.Create<Startup>())
             {
-                var response = await server.HttpClient.GetAsync("/Me.jpg");
-                var contenttype = response.Content.Headers.ContentType.MediaType;
-                Assert.AreEqual("image/jpeg", contenttype);
+                return await callback(server.HttpClient);
             }
         }
     }
