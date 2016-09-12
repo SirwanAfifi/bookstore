0d4548b5a9b4075560c6c6d30b86a7e008cec935
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-09-12 23:58:08 +0430

    Added test project

---
+++ servers/asp.net/Owin.Demo.Hosting/Owin.Demo.Tests/OwinTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Owin.Demo.Hosting;
+
+namespace Owin.Demo.Tests
+{
+    [TestClass]
+    public class OwinTests
+    {
+        
+    }
+}
