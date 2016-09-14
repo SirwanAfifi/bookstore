9b09e6530391b34c4b601927a75d6094d0f5acf9
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-09-14 22:24:42 +0430

    added simple Owin app

---
+++ servers/asp.net/Owin.Demo_Console_App/Owin.Demo_Console_App/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Owin.Demo_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "http://localhost:8080";
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Server started");
+                Console.ReadLine();
+                Console.WriteLine("Server stopped!");
+            }
+
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+            app.UseWelcomePage();
+            app.Run(ctx =>
+            {
+                throw new Exception();
+                return ctx.Response.WriteAsync("Hello");
+            });
+        }
+    }
+}
