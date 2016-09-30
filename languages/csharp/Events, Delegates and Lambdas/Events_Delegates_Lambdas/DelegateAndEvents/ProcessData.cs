f567274ed3f57e26da08b97a1e2c96648cfd4f84
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-09-30 16:53:07 +0330

    using lambdas

---
+++ languages/csharp/Events, Delegates and Lambdas/Events_Delegates_Lambdas/DelegateAndEvents/ProcessData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DelegateAndEvents
+{
+    public class ProcessData
+    {
+        public void Process(int x, int y, BizRuleDelegate del)
+        {
+            var result = del(x, y);
+            Console.WriteLine(result);
+        }
+    }
+}
--- languages/csharp/Events, Delegates and Lambdas/Events_Delegates_Lambdas/DelegateAndEvents/Program.cs
+++ languages/csharp/Events, Delegates and Lambdas/Events_Delegates_Lambdas/DelegateAndEvents/Program.cs
@@ -2,10 +2,18 @@
 
 namespace DelegateAndEvents
 {
+    public delegate int BizRuleDelegate(int x, int y);
+
     class Program
     {
         static void Main(string[] args)
         {
+            BizRuleDelegate addDel = (x, y) => x + y;
+            BizRuleDelegate multiplyDel = (x, y) => x * y;
+
+            var data = new ProcessData();
+            data.Process(2, 3, (x, y) => x * y);
+
             var worker = new Worker();
             worker.WorkPerformed += (s, e) => Console.WriteLine(e.Hours + " " + e.WorkType);
             worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is done");
