9c58ee519851e828db1f082833eace8331db757f
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-09-30 15:28:19 +0330

    Using Labmda

--- languages/csharp/Events, Delegates and Lambdas/Events_Delegates_Lambdas/DelegateAndEvents/Program.cs
+++ languages/csharp/Events, Delegates and Lambdas/Events_Delegates_Lambdas/DelegateAndEvents/Program.cs
@@ -7,13 +7,8 @@ namespace DelegateAndEvents
         static void Main(string[] args)
         {
             var worker = new Worker();
-            worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e)
-            {
-                Console.WriteLine(e.Hours + " " + e.WorkType);
-            };
-            worker.WorkCompleted += delegate {
-                Console.WriteLine("Worker is done");
-            };
+            worker.WorkPerformed += (s, e) => Console.WriteLine(e.Hours + " " + e.WorkType);
+            worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is done");
             worker.DoWork(10, WorkType.Gold);
             Console.Read();
         }
