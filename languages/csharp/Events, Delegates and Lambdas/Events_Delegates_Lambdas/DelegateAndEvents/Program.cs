33fefab462f1411aff5b8efb31e4a4f5b56ea386
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-09-30 17:47:35 +0330

    using action

--- languages/csharp/Events, Delegates and Lambdas/Events_Delegates_Lambdas/DelegateAndEvents/Program.cs
+++ languages/csharp/Events, Delegates and Lambdas/Events_Delegates_Lambdas/DelegateAndEvents/Program.cs
@@ -14,6 +14,10 @@ namespace DelegateAndEvents
             var data = new ProcessData();
             data.Process(2, 3, (x, y) => x * y);
 
+            Action<int, int> myAction = (x, y) => Console.WriteLine(x + y);
+            Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x * y);
+            data.ProcessAction(2, 3, myAction);
+
             var worker = new Worker();
             worker.WorkPerformed += (s, e) => Console.WriteLine(e.Hours + " " + e.WorkType);
             worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is done");
