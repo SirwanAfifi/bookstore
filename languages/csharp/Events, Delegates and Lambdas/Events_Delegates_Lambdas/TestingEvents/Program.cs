0a1081b1fcab600e8dd97140de0505f2a97b2d8a
Sirwa <Sirwa@DESKTOP-RFT7KH6>
2016-10-11 14:05:55 +0330

    Added Test Console Application

---
+++ languages/csharp/Events, Delegates and Lambdas/Events_Delegates_Lambdas/TestingEvents/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CommunicatingBetweenControls;
+using CommunicatingBetweenControls.Models;
+
+namespace TestingEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object Sharing doesn't support multiple process
+            // more info: http://stackoverflow.com/a/10016102/1646540
+            //Mediator.GetInstance().OnJobChanged(null, new Job { Title = "FFF", EndDate = DateTime.MaxValue, ID = 1, StartDate = DateTime.MinValue });
+            Console.Read();
+        }
+    }
+}
